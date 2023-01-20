785f055a1f7ddd30fa307f5de7f65e4e3748c26f
“Will <wwh22@uw.edu>
2023-01-20 14:20:04 -0800

    Add a simple instruction modal

--- src/main.js
+++ src/main.js
@@ -248,3 +248,22 @@ window.addEventListener("resize", function () {
   chart.resize();
   protein.stage.handleResize();
 });
+
+// Modal UI
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
